05f6baf04821e272b28be4e564ee61485048bb64
Icery <gamingderk@gmail.com>
2015-09-28 13:42:21 +0200

    Starting Engine Project

--- src/net/keinleben/localhost/engine/Engine.java
+++ src/net/keinleben/localhost/engine/Engine.java
@@ -1,10 +1,19 @@
 package net.keinleben.localhost.engine;
 
+import org.bukkit.plugin.java.JavaPlugin;
+
 /**
  * [- Author: Felix / Froznshard -]
  */
-public class Engine {
+public class Engine extends JavaPlugin {
 
+    @Override
+    public void onEnable() {
+        super.onEnable();
+    }
 
-
+    @Override
+    public void onDisable() {
+        super.onDisable();
+    }
 }
--- src/net/keinleben/localhost/engine/system/System.java
+++ src/net/keinleben/localhost/engine/system/System.java
@@ -4,4 +4,41 @@ package net.keinleben.localhost.engine.system;
  * [- Author: Felix / Froznshard -]
  */
 public class System {
+
+    private static String mRam,cRam,fRam;
+    private static String r1 = "[FREE RAM] ", r2 = "[CURRENT USAGE] ", r3 = "[MAX RAM] ";
+    private static String consoleLine = "---------------------------------------------------";
+    private static java.lang.System s;
+
+    public System(){
+        mRam = String.valueOf(Runtime.getRuntime().maxMemory() / 1024L / 1024L);
+        cRam = String.valueOf((Runtime.getRuntime().maxMemory() - Runtime.getRuntime().freeMemory()) / 1024L / 1024L);
+        fRam = String.valueOf(Runtime.getRuntime().freeMemory() / 1024L / 1024L);
+    }
+
+    public static String getFreeRam(){
+        return fRam;
+    }
+
+    public static String getMaxRam(){
+        return mRam;
+    }
+
+    public static String getCurrentRamUsage(){
+        return cRam;
+    }
+
+    public static void compactInfo(boolean form){
+        if (form){
+            s.out.println(consoleLine);
+            s.out.println(r1 + fRam + " | " + r2 + cRam + " | " +r3 + mRam);
+            s.out.println(consoleLine);
+        }else {
+            s.out.println("Ram Notice");
+            s.out.println("Usage: " + cRam);
+            s.out.println("Free: " + fRam);
+            s.out.println("Max: " + mRam);
+        }
+    }
+
 }
